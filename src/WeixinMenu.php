b7166ce546baa560e40f7d80246a19326258b5d6
wang jiankang <wjk@duofrom.com>
2019-04-01 14:49:24 +0800

    bugfix

--- src/WeixinMenu.php
+++ src/WeixinMenu.php
@@ -1,8 +1,9 @@
 <?php
 
+namespace VRobin\Weixin;
 /**
  * 微信自定义菜单辅助类
- * 
+ *
  * @author Viking Robin <admin@vkrobin.com>
  */
 Class WeixinMenu
@@ -12,10 +13,10 @@ Class WeixinMenu
 
     /**
      * 创建一级菜单
-     * 
+     *
      * @param string $name 菜单标题，不超过16个字节，子菜单不超过40个字节
-     * @param string $type 菜单的响应动作类型，目前有click、view两种类型 
-     * @param string $value  若为click类型,则为key的值，若为view类型，则为url链接
+     * @param string $type 菜单的响应动作类型，目前有click、view两种类型
+     * @param string $value 若为click类型,则为key的值，若为view类型，则为url链接
      * @return boolean 返回是否创建成功
      */
     public function addButton($name, $type = NULL, $value = NULL)
@@ -36,11 +37,11 @@ Class WeixinMenu
 
     /**
      * 创建二级菜单
-     * 
+     *
      * @param int $index 父按钮序号 1~3 之间
      * @param string $name 菜单标题，不超过16个字节，子菜单不超过40个字节
-     * @param string $type 菜单的响应动作类型，目前有click、view两种类型 
-     * @param string $value  若为click类型,则为key的值，若为view类型，则为url链接
+     * @param string $type 菜单的响应动作类型，目前有click、view两种类型
+     * @param string $value 若为click类型,则为key的值，若为view类型，则为url链接
      * @return boolean 返回是否创建成功
      */
     public function addSubButton($index, $name, $type, $value)
@@ -66,7 +67,7 @@ Class WeixinMenu
 
     /**
      * 导出数组
-     * 
+     *
      * @return array
      */
     public function toArray()
